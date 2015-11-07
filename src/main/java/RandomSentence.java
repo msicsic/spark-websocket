b11f9aea52ab073cfa7a9f63738b5adfe393bb0d
tipsy <davidaase@hotmail.com>
2015-11-07 21:53:31 +0100

    Refactor sentence-generator

--- src/main/java/RandomSentence.java
+++ src/main/java/RandomSentence.java
@@ -1,27 +1,19 @@
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class RandomSentence {
 
+    private static String[] name      = "Alice_Bob|Alex|Nina|Jen|Carl|Jill|Jack|Ben|Rose|John|Max|Joe|Kim".split("\\|");
+    private static String[] action    = "wants to|used to|likes to|often tries to|decided not to|decided to|doesn't want to|is learning how to".split("\\|");
+    private static String[] activity  = "eat|dance|sleep|go to burger king|vacuum|mountain climb|stalk people|fly kites|bark".split("\\|");
+    private static String[] prepish   = "with|without|on top of|underneath|next to|on|over|with the help of|in spite of|disguised as|because of".split("\\|");
+    private static String[] adjective = "dry|wet|dead|giant|tiny|orange|cloudy|rude|obnoxious|starving".split("\\|");
+    private static String[] noun      = "cranberries|racecars|spaceships|dandelions|leaves|windmills|puppets|aliens|tigers|mountains".split("\\|");
+
     public static String get() {
-        return random(name())      + " " +
-               random(action())    + " " +
-               random(activity())  + " " +
-               random(prepish())   + " " +
-               random(adjective()) + " " +
-               random(noun());
+        return String.join(" ", get(name), get(action), get(activity), get(prepish), get(adjective), get(noun));
     }
 
-    public static List<String> name()      { return Arrays.asList("Alice", "Bob", "Alex", "Nina", "Jen", "Carl", "Jill", "Jack", "Ben", "Rose", "John", "Max", "Joe", "Kim"); }
-    public static List<String> action()    { return Arrays.asList("wants to", "used to", "likes to", "often tries to", "decided not to", "decided to", "doesn't want to", "is learning how to"); }
-    public static List<String> activity()  { return Arrays.asList("eat", "dance", "sleep", "go to burger king", "vacuum", "mountain climb", "stalk people", "fly kites", "bark"); }
-    public static List<String> prepish()   { return Arrays.asList("with", "without", "on top of", "underneath", "next to", "on", "over", "with the help of", "in spite of", "disguised as", "because of"); }
-    public static List<String> adjective() { return Arrays.asList("dry", "wet", "dead", "giant", "tiny", "orange", "cloudy", "rude", "obnoxious", "starving"); }
-    public static List<String> noun()      { return Arrays.asList("cranberries", "racecars", "spaceships", "dandelions", "leaves", "windmills", "puppets", "aliens", "tigers", "mountains"); }
-
-    public static String random(List<String> list) {
-        Collections.shuffle(list);
-        return list.get(0);
+    public static String get(String[] words) {
+        return words[new Random().nextInt(words.length)];
     }
 }
