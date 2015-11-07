ee72a1e69bdce47dad6cec9a14c6d566696a4e73
tipsy <davidaase@hotmail.com>
2015-11-07 16:55:56 +0100

    Make it work

--- src/main/java/FeedListener.java
+++ src/main/java/FeedListener.java
@@ -1,30 +1,22 @@
-import javafx.collections.*;
 import org.eclipse.jetty.websocket.api.*;
 import org.eclipse.jetty.websocket.api.annotations.*;
-import java.io.*;
-import java.text.*;
-import java.util.*;
-import static j2html.TagCreator.*;
 
 @WebSocket
 public class FeedListener {
 
     private Session session;
-    private ListChangeListener listChangeListener = c -> {
-        send(createHtmlMessage(c.toString()));
-    };
 
     @OnWebSocketConnect
     public void onConnect(Session session) throws Exception {
         System.out.println(session.getRemoteAddress().toString() + " connected");
         this.session = session;
-        Main.observableList.addListener(listChangeListener);
+        Main.wsSessions.add(this.session);
     }
 
     @OnWebSocketClose
     public void onClose(int statusCode, String reason) {
+        Main.wsSessions.remove(this.session);
         this.session = null;
-        Main.observableList.removeListener(listChangeListener);
     }
 
     @OnWebSocketMessage
@@ -32,22 +24,6 @@ public class FeedListener {
         switch (message) {
             case "SLOW": Main.setTimerSpeed(2500); break;
             case "FAST": Main.setTimerSpeed(500);  break;
-        }
-    }
-
-    private String createHtmlMessage(String text) {
-        return div().with(
-                text(new SimpleDateFormat("HH:mm:ss (dd. MMM, yyyy)").format(new Date())),
-                br(),
-                text(text)
-        ).render();
-    }
-
-    private void send(String string) {
-        try {
-            session.getRemote().sendString(string);
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,14 @@
-import javafx.collections.*;
+import org.eclipse.jetty.websocket.api.*;
+import java.io.*;
+import java.text.*;
 import java.util.*;
+import static j2html.TagCreator.*;
 import static spark.Spark.*;
 
 public class Main {
 
     static Timer feedTimer = new Timer();
-    static ObservableList<String> observableList = FXCollections.observableList(new ArrayList<>());
+    static List<Session> wsSessions = new ArrayList<>();
 
     public static void main(String[] args) {
         setTimerSpeed(2500);
@@ -20,9 +23,29 @@ public class Main {
         feedTimer = new Timer();
         feedTimer.scheduleAtFixedRate(new TimerTask() {
             public void run() {
-                observableList.add(RandomSentence.get());
+                send(createHtmlMessage(RandomSentence.get()));
             }
         }, intervalInMillis, intervalInMillis); //delay, interval
+    }
+
+    private static void send(String string) {
+        try {
+            for(Session session : Main.wsSessions) {
+                if(session.isOpen()) {
+                    session.getRemote().sendString(string);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String createHtmlMessage(String text) {
+        return div().with(
+                text(new SimpleDateFormat("HH:mm:ss (dd. MMM, yyyy)").format(new Date())),
+                br(),
+                text(text)
+        ).render();
     }
 
 }
