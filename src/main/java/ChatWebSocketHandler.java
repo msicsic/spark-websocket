e485733150a11139f70f370c3b1b34fe6deb1d9d
tipsy <davidaase@hotmail.com>
2015-11-08 16:35:19 +0100

    Simplify username handling, rename methods

--- src/main/java/ChatWebSocketHandler.java
+++ src/main/java/ChatWebSocketHandler.java
@@ -7,18 +7,19 @@ public class ChatWebSocketHandler {
     @OnWebSocketConnect
     public void onConnect(Session currentUser) throws Exception {
         Main.currentUsers.add(currentUser);
-        Main.sendToAll(currentUser, "I have arrived!");
+        Main.usernameMap.put(currentUser, "User" + Main.nextUserNumber++);
+        Main.broadcastMessageFromUser(currentUser, "I have arrived!");
     }
 
     @OnWebSocketClose
     public void onClose(Session currentUser, int statusCode, String reason) {
         Main.currentUsers.remove(currentUser);
-        Main.sendToAll(currentUser, "I'm outta here!");
+        Main.broadcastMessageFromUser(currentUser, "I'm outta here!");
     }
 
     @OnWebSocketMessage
     public void onMessage(Session currentUser, String message) {
-        Main.sendToAll(currentUser, message);
+        Main.broadcastMessageFromUser(currentUser, message);
     }
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,21 +10,21 @@ public class Main {
 
     static List<Session> currentUsers = new ArrayList<>();
     static Map<Session, String> usernameMap = new HashMap<>();
-    static int usernamesGenerated;
+    static int nextUserNumber = 1; //Assign to username for next connecting user
 
     public static void main(String[] args) {
-        staticFileLocation("public"); // index.html will be served at localhost:4567 (default port)
+        staticFileLocation("public"); //index.html will be served at localhost:4567 (default port)
         webSocket("/chat", ChatWebSocketHandler.class);
         init();
     }
 
     //Sends a message from one user to all users, along with a list of current users
-    public static void sendToAll(Session msgSource, String message) {
+    public static void broadcastMessageFromUser(Session user, String message) {
         currentUsers.stream().filter(Session::isOpen).forEach(session -> {
             try {
                 session.getRemote().sendString(String.valueOf(new JSONObject()
-                    .put("userMessage", createHtmlMessage(msgSource, message)) //The message wrapped in HTML
-                    .put("userlist", currentUsers.stream().map(Main::getUsername).collect(Collectors.toList()))
+                    .put("userMessage", createHtmlMessageFromUser(user, message))
+                    .put("userlist", currentUsers.stream().map(usernameMap::get).collect(Collectors.toList()))
                 ));
             } catch (Exception e) {
                 e.printStackTrace();
@@ -33,20 +33,12 @@ public class Main {
     }
 
     //Builds a HTML element with username, message and timestamp,
-    private static String createHtmlMessage(Session msgSource, String message) {
+    private static String createHtmlMessageFromUser(Session user, String message) {
         return article().with(
-                b(getUsername(msgSource) + " says:"),
+                b(usernameMap.get(user) + " says:"),
                 p(message),
                 span().withClass("timestamp").withText(new SimpleDateFormat("HH:mm:ss").format(new Date()))
         ).render();
-    }
-
-    //Create and return username for session
-    public static String getUsername(Session user) {
-        if(usernameMap.get(user) == null) {
-            usernameMap.put(user, "User" + (++usernamesGenerated)); //Pre-increment username
-        }
-        return usernameMap.get(user);
     }
 
 }
