4305d7940f33138b487fc603e2fb755b1f92873c
tipsy <davidaase@hotmail.com>
2015-11-08 12:57:25 +0100

    Formatting

--- src/main/resources/public/websocketDemo.js
+++ src/main/resources/public/websocketDemo.js
@@ -1,21 +1,21 @@
 //Establish the WebSocket connection and set up event handlers
-var webSocket = new WebSocket("ws://"+location.hostname+":"+location.port+"/chat/");
-webSocket.onmessage = function(msg) { updateChat(msg); };
-webSocket.onclose = function(){alert("WebSocket connection closed")};
+var webSocket = new WebSocket("ws://" + location.hostname + ":" + location.port + "/chat/");
+webSocket.onmessage = function (msg) { updateChat(msg); };
+webSocket.onclose = function () { alert("WebSocket connection closed") };
 
 //Send message if "Send" is clicked
-id("send").addEventListener("click", function() {
+id("send").addEventListener("click", function () {
     sendMessage(id("message").value);
 });
 
 //Send message if enter is pressed in the input field
-id("message").addEventListener("keypress", function(e) {
-    if(e.keyCode === 13){ sendMessage(e.target.value); }
+id("message").addEventListener("keypress", function (e) {
+    if (e.keyCode === 13) { sendMessage(e.target.value); }
 });
 
 //Send a message if it's not empty, then clear the input field
 function sendMessage(message) {
-    if(message !== "") {
+    if (message !== "") {
         webSocket.send(message);
         id("message").value = "";
     }
